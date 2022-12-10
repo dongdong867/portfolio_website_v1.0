fae4fa401aa9cec83459c8dd7f7483dc30c1dbab
dongdong <93483694+dongdong867@users.noreply.github.com>
2022-12-11 00:47:07 +0800

    feat: Combine nccu-job-bank components

--- pages/works/nccu-job-bank.vue
+++ pages/works/nccu-job-bank.vue
@@ -1,7 +1,25 @@
 <template>
-	<div></div>
+	<div
+		class="w-screen h-[87vh] bg-scroll bg-no-repeat bg-[length:100vw_auto] overflow-y-auto overflow-x-hidden select-none cursor-default snap-y snap-mandatory scroll-pb-[5vh] scroll-pt-[8vh]"
+		:style="{ 'background-image': 'url(' + NCCUJobBankView + ')' }"
+	>
+		<div class="w-screen flex flex-col text-3xl font-bold">
+			<NccuJobBankTitle />
+			<div class="w-screen h-full mt-[22vh] bg-base text-2xl">
+				<NccuJobBankOverview />
+				<NccuJobBankBackground />
+				<NccuJobBankObjective />
+				<NccuJobBankProblems />
+				<NccuJobBankShowcase />
+			</div>
+		</div>
+	</div>
 </template>
 
-<script>
-export default {}
+<script setup>
+import NCCUJobBankView from '~/assets/images/nccu-job-bank/title.png?url'
+
+definePageMeta({
+	layout: 'work'
+})
 </script>
