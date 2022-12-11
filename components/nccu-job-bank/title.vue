72c6005a5645c37d7527cb567aa44a898462f883
dongdong <93483694+dongdong867@users.noreply.github.com>
2022-12-11 17:32:32 +0800

    fix: Fix margin error

--- components/nccu-job-bank/title.vue
+++ components/nccu-job-bank/title.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="work-title">
 		<div class="text-7xl font-extrabold">NCCU Job Bank</div>
-		<div class="mt-5">Full Stack Developer</div>
+		<div class="mt-[5vh]">Full Stack Developer</div>
 		<div>2022/01 - 2022/06</div>
 	</div>
 </template>
