7fbcd2bbce6980ef3bbc78ddfe39a444d4d253e2
dongdong <93483694+dongdong867@users.noreply.github.com>
2022-12-11 17:20:58 +0800

    feat: Build lets-beat-google page

---
+++ components/lets-beat-google/objective.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="mt-[50vh] snap-center snap-always">
+		<div class="w-max m-auto">TIME LINE</div>
+		<div class="m-auto text-[80px] gradient-text">3 months</div>
+		<div class="w-max m-auto mt-[2vh]">BUILD WITH</div>
+		<div class="text-[80px]">
+			<div class="flex place-content-evenly">
+				<div class="gradient-text">Nuxt3.js</div>
+				<div class="gradient-text">TailwindCSS</div>
+			</div>
+			<div class="gradient-text m-auto mt-[1vh]">Springboots</div>
+		</div>
+		<div class="work-border-l">
+			<div class="text-5xl font-extrabold mt-[10vh]">Objective</div>
+			<div class="mt-[5vh]">
+				Out team decides to optimized the search resukt based on Google, to improve the cons of
+				search engine in all topics, we will focus on specific topics to make search results have a
+				higher accuracy.
+			</div>
+		</div>
+	</div>
+</template>
--- pages/works/lets-beat-google.vue
+++ pages/works/lets-beat-google.vue
@@ -1,7 +1,22 @@
 <template>
-	<div></div>
+	<div class="work-basic" :style="{ 'background-image': 'url(' + a + ')' }">
+		<div class="w-screen flex flex-col text-2xl font-bold">
+			<LetsBeatGoogleTitle />
+			<div class="mt-[22vh] bg-base">
+				<LetsBeatGoogleOverview class="w-[80vw] m-auto" />
+				<div class="w-[70vw] m-auto">
+					<LetsBeatGoogleBackground />
+					<LetsBeatGoogleObjective />
+					<LetsBeatGoogleProblems />
+				</div>
+				<LetsBeatGoogleShowcase />
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-export default {}
+definePageMeta({
+	layout: 'work'
+})
 </script>
