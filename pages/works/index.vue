283a8ce31e5c985d18ae6c4391918241f66998e7
dongdong <93483694+dongdong867@users.noreply.github.com>
2022-12-11 23:30:38 +0800

    feat: Add links to work page

--- pages/works/index.vue
+++ pages/works/index.vue
@@ -1,10 +1,10 @@
 <template>
 	<div class="w-screen h-full flex flex-col gap-y-[10vh]">
-		<WorksOpenNCCU />
-		<WorksLetsBeatGoogle />
-		<WorksNCCUJobBank />
-		<WorksTranslatorIntroduction />
-		<WorksScoreCalculator />
+		<nuxt-link to="works/open-nccu"><WorksOpenNCCU /></nuxt-link>
+		<nuxt-link to="works/lets-beat-google"><WorksLetsBeatGoogle /></nuxt-link>
+		<nuxt-link to="works/nccu-job-bank"><WorksNCCUJobBank /></nuxt-link>
+		<nuxt-link to="works/translator-introduction"><WorksTranslatorIntroduction /></nuxt-link>
+		<nuxt-link to="works/score-calculator"><WorksScoreCalculator /></nuxt-link>
 	</div>
 </template>
 
