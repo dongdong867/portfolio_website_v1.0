cab9f54632ccfd88c68171d3a8ab157c00bd591b
dongdong <93483694+dongdong867@users.noreply.github.com>
2022-12-20 03:02:21 +0800

    feat: Add open-nccu showcase

---
+++ components/open-nccu/showcase/main.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="mb-[20vh] mt-[10vh] showcase-basic">
+		<div class="mb-[10vh] pl-[10vw] flex-con-end place-items-end mx-[5vw]">
+			<div class="text-6xl gradient-text flex-con-end place-items-end">
+				<div>Main Page</div>
+			</div>
+			<div class="w-[40vw] mt-[1vh] text-right">Main page of the web application.</div>
+		</div>
+		<img :src="Main" class="img-size place-items-center" />
+	</div>
+</template>
+
+<script setup>
+import Main from '~/assets/images/open-nccu/main-frame.png?url'
+</script>
+
+<style lang="scss" scoped></style>
---
+++ components/open-nccu/showcase/s-n-calculator.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="my-[20vh] showcase-basic">
+		<div class="mb-[10vh] pl-[10vw] flex-con-end place-items-end mx-[5vw]">
+			<div class="text-6xl gradient-text flex-con-end place-items-end">
+				<div>South or North</div>
+				<div>Building Page</div>
+				<div>(Calculator ver.)</div>
+			</div>
+			<div class="w-[40vw] mt-[1vh] text-right">A page to avoid lossing in school building.</div>
+		</div>
+		<img :src="Calculator" class="img-size place-items-center" />
+	</div>
+</template>
+
+<script setup>
+import Calculator from '~/assets/images/open-nccu/s-n-calculator.png?url'
+</script>
+
+<style lang="scss" scoped></style>
